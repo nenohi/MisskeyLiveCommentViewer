96e642abba306fba0de25ffd3034c4c4e4055a0f
nenohi <kimutipartylove@gmail.com>
2022-09-25 07:30:00 +0900

    Deleted not using

--- MisskeyCommentViewer/Misskey.cs
+++ MisskeyCommentViewer/Misskey.cs
@@ -1,11 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Net;
-using System.Net.Sockets;
 using System.Net.WebSockets;
 
 namespace MisskeyCommentViewer
