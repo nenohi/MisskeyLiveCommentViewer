5a696c359a92bcea761adb432dd2b46961a2da2f
nenohi <kimutipartylove@gmail.com>
2022-09-24 23:51:19 +0900

    不要なusing削除

--- MisskeyCommentViewer/MisskeyCommentViewer.cs
+++ MisskeyCommentViewer/MisskeyCommentViewer.cs
@@ -1,11 +1,7 @@
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
